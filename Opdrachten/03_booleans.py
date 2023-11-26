28e1c308586a34b29d7d8013bb9566219dc757af
markdackus247 <79475320+markdackus247@users.noreply.github.com>
2023-11-26 12:20:20 +0100

    Create 03_booleans.py

---
+++ Opdrachten/03_booleans.py
@@ -0,0 +1,7 @@
+behaald_cijfer = 6.7
+gemiddelde = 6.2
+boven_gemiddelde = behaald_cijfer > gemiddelde
+
+print("Behaalde cijfer is boven het gemiddelde: ", boven_gemiddelde)
+
+print("Type boven_gemiddelde: ", type(boven_gemiddelde))
