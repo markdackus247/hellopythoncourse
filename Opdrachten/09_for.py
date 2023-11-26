43fefed31f8b43f09eb2d462217d05ed7965656c
markdackus247 <79475320+markdackus247@users.noreply.github.com>
2023-11-26 21:33:19 +0100

    Opdracht 9 en 10 gemaakt.

---
+++ Opdrachten/09_for.py
@@ -0,0 +1,12 @@
+"""
+Author: Mark Dackus
+Date: 26-11-2023
+Script: for loop
+"""
+bestanden_aan_te_maken = ["read.me", "license", "app.js"]
+
+for bestandsnaam in bestanden_aan_te_maken:
+    print("Bestandsnaam: ", bestandsnaam)
+    
+    with open(bestandsnaam, 'w') as f:
+        f.write('Dit is het bestand ' + bestandsnaam)
---
+++ Opdrachten/10_while.py
@@ -0,0 +1,18 @@
+"""
+Author: Mark Dackus
+Date: 26-11-2023
+Script: while
+"""
+tafel_van = 10
+invoer_juist = False
+while not invoer_juist:
+    tafel_van_input = input("Van welk getal wil je het tafeltje: (tussen 1 en 100): ")
+    if (tafel_van_input.isdigit()):
+        tafel_van = int(tafel_van_input)
+        invoer_juist = True
+
+start = 1
+while (start < 10):
+    start = start + 1
+    antwoord = start * tafel_van
+    print(f"{start} x {tafel_van} = {antwoord}")
