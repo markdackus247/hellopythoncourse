67782a9ab054e2d338766de6c6a30cfca4894539
markdackus247 <79475320+markdackus247@users.noreply.github.com>
2023-11-29 19:00:20 +0100

    Update 09_for.py

--- Opdrachten/09_for.py
+++ Opdrachten/09_for.py
@@ -3,6 +3,8 @@ Author: Mark Dackus
 Date: 26-11-2023
 Script: for loop
 """
+# Let op! Als je dit script voor de tweede keer runt krijg je een foutmelding
+# omdat de bestanden al bestaan. Verwijder dan eerst de aangemaakte bestanden.
 bestanden_aan_te_maken = ["read.me", "license", "app.js"]
 
 for bestandsnaam in bestanden_aan_te_maken:
